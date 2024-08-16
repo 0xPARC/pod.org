7efcdffd8b6226998932d477533d71faf5663581
Richard Liu <richard.y.liu@berkeley.edu>
2024-08-16 00:31:02 -0700

    fix: meerkat link

--- app/layout.tsx
+++ app/layout.tsx
@@ -170,7 +170,7 @@ export default function Layout({ children }: Props) {
                   Zupass
                 </Link>
                 <Link
-                  href="https://forum.devcon.org/c/dip-discussion/6"
+                  href="https://forum.devcon.org/t/meerkat-q-a-tool-for-audience-engagement/3805"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:bg-gray-100 inline-flex items-center justify-center whitespace-nowrap rounded-md text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
