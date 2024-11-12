378ca8a161678ad6d29fcfdbe1d7b60618412e37
Rob Knight <mail@robknight.org.uk>
2024-11-12 17:50:13 +0100

    New site (#4)
    * Add starlight-based site outline
    
    * Tweak homepage metadata
    
    * Add home page, docs index, partial Z API introduction
    
    * Starting point for POD + GPC docs #1
    
    * Finish basic POD+GPC content #2
    
    - GPC Getting Started (#2)
    - POD + GPC examples pages
    - Coming Soon stubs for other POD+GPC pages
    
    * Add FrogJuice links
    
    * Z API introduction
    
    * Remove unused menu entries
    
    * Index page and Z-API Examples (#3)
    
    * Index page
    
    * Z-API examples page
    
    * Support sitemap
    
    * Set output dir
    
    ---------
    
    Co-authored-by: Andrew Twyman <artwyman@gmail.com>
    Co-authored-by: Andrew Twyman <artwyman@users.noreply.github.com>

---
+++ astro.config.mjs
@@ -0,0 +1,74 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	server: {
+    port: 4374
+  },
+	outDir: "./out",
+	site: 'https://pod.org',
+	integrations: [
+		starlight({
+      title: "pod.org",
+      social: {
+        github: "https://github.com/proofcarryingdata/zupass",
+        "x.com": "https://twitter.com/zupassproject",
+        telegram: "https://t.me/zupass"
+      },
+			components: {
+        SiteTitle: "./src/components/starlight/SiteTitle.astro"
+      },
+			sidebar: [
+				{
+					label: 'Home',
+					slug: 'docs',
+				},
+				{
+					label: 'POD',
+					items: [
+						{ label: 'Introduction', slug: 'pod/introduction' },
+						{ label: 'Getting Started', slug: 'pod/getting-started' },
+						{ label: 'POD Names and Schemas', slug: 'pod/names' },
+						{ label: 'POD Values and Types', slug: 'pod/values' },
+						{ label: 'Examples', slug: 'pod/examples' },
+						{ label: 'Developer Resources', slug: 'pod/resources' },
+						{ label: 'Disclaimers', slug: 'pod/disclaimers' },
+					]
+				},
+				{
+					label: 'GPC',
+					items: [
+						{ label: 'Introduction', slug: 'gpc/introduction' },
+						{ label: 'Getting Started', slug: 'gpc/getting-started' },
+						{ label: 'Proof Configuration', slug: 'gpc/proof-configuration' },
+						{ label: 'Identity and Ownership', slug: 'gpc/identity-ownership' },
+						{ label: 'Verifying Securely', slug: 'gpc/verification' },
+						{ label: 'Circuit Artifacts', slug: 'gpc/artifacts' },
+						{ label: 'Examples', slug: 'gpc/examples' },
+						{ label: 'Developer Resources', slug: 'gpc/resources' },
+						{ label: 'Disclaimers', slug: 'gpc/disclaimers' },
+					]
+				},
+				{
+					label: 'Z API',
+					items: [
+						{ label: 'Introduction', slug: 'z-api/introduction' },
+						{ label: 'Getting Started', slug: 'z-api/getting-started' },
+						{ label: 'Queries', slug: 'z-api/queries' },
+						{ label: 'Ticket Proofs', slug: 'z-api/ticket-proofs' },
+						{ label: 'Examples', slug: 'z-api/examples' }
+					]
+				},
+				{
+					label: 'POD extensions',
+					autogenerate: { directory: 'pod-extensions' },
+				}
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
